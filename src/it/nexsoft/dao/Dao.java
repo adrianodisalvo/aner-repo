c222d7d2faa646d5e4ad6553071d7a9d3d4085e5
Unknown <adro.disalvo@fastwebnet.it>
2018-03-22 15:12:49 +0100

    Dao simplified

--- src/it/nexsoft/dao/Dao.java
+++ src/it/nexsoft/dao/Dao.java
@@ -1,20 +1,18 @@
 package it.nexsoft.dao;
 
-import java.lang.reflect.ParameterizedType;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 public abstract class Dao<E> implements IDao<E> {
 	
-	protected Class entityClass;
+	//protected Class entityClass;
 
 	@PersistenceContext
 	protected EntityManager entityManager;
 
 	public Dao() {
-		ParameterizedType genericSuperclass = (ParameterizedType)getClass().getGenericSuperclass();
-		this.entityClass = (Class)genericSuperclass.getActualTypeArguments()[1];
+		//ParameterizedType genericSuperclass = (ParameterizedType)getClass().getGenericSuperclass();
+		//this.entityClass = (Class)genericSuperclass.getActualTypeArguments()[1];
 	}
 
 	public void persist(E entity) { entityManager.persist(entity); }
